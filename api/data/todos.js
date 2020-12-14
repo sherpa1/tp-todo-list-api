c395fd8c48b0390354f13a5192f2d947eca5e719
Alexandre Leroux <alex@sherpa.one>
2020-12-14 13:42:11 +0100

    external data

---
+++ api/data/todos.js
@@ -0,0 +1,54 @@
+const todos = [
+    {
+        id:1,
+        title:"Lorem ipsum dolor sit amet",
+        content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        done:false,
+        deadline:null,
+        created_at:"2020-12-10 23:00:00",
+        updated_at:null,
+        user_id:1
+    },
+    {
+        id:2,
+        title:"Consectetur adipiscing elit",
+        content:"Maecenas tincidunt consequat commodo. Proin quam ante, venenatis ac eros et, scelerisque luctus odio.",
+        done:false,
+        deadline:null,
+        created_at:"2020-12-10 23:10:00",
+        updated_at:null,
+        user_id:1
+    },
+    {
+        id:3,
+        title:"Nullam ullamcorper a mauris sit amet mattis. ",
+        content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        done:false,
+        deadline:null,
+        created_at:"2020-12-10 23:20:00",
+        updated_at:null,
+        user_id:1
+    },
+    {
+        id:4,
+        title:"Vestibulum magna elit, rhoncus eu sapien ut",
+        content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        done:false,
+        deadline:"2020-12-11 09:10:00",
+        created_at:"2020-12-10 23:30:00",
+        updated_at:"2002-12-11 07:00:00",
+        user_id:1
+    },
+    {
+        id:5,
+        title:"Faucibus accumsan magna.",
+        content:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        done:true,
+        deadline:null,
+        created_at:"2020-12-10 23:40:00",
+        updated_at:null,
+        user_id:1
+    },
+];
+
+module.exports = todos;
